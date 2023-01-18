6344ddca8afd7f3022dac1da7e3988e273592cf2
ABY <abyener54@gmail.com>
2023-01-18 21:55:56 +0300

    Add react native navigator

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -4,6 +4,7 @@ import tw from "tailwind-react-native-classnames";
 import { Icon } from "@rneui/themed";
 import MapScreen from "../assets/images/nav/car.webp";
 import EatScreen from "../assets/images/nav/food.png";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -21,13 +22,18 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
+
   return (
     <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
           <View>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
---
+++ screens/MapScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+const MapScreen = () => {
+  return (
+    <View style={tw`p-5`}>
+      <Text>Map Screen</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
